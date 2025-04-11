cfe5dbf141d656ef6193d07aa8c922468a22ade2
angelddeleon <angeldeleonpr00@gmail.com>
2025-04-11 16:04:06 -0400

    config database para despliegue

--- src/server/config/db.js
+++ src/server/config/db.js
@@ -2,6 +2,19 @@ import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
     host: "localhost",
+    user: "waikikip_admin",
+    password: "*Qe0_$Z%XwIY" ,
+    database: "waikikip_waikiki",
+    waitForConnections: true,
+    connectionLimit: 50,
+    queueLimit: 0,
+});
+
+export default pool;
+
+/**
+ const pool = mysql.createPool({
+    host: "localhost",
     user: "root",
     password: "#Lo28de06" ,
     database: "waikiki",
@@ -10,16 +23,5 @@ const pool = mysql.createPool({
     queueLimit: 0,
 });
 
-export default pool;
-
-/*
-const pool = mysql.createPool({
-    host: "localhost",
-    user: "waikikip_Admin", 
-    password: "3xU06)sn0TDH" ,
-    database: "waikikip_padel",
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-});
-*/
+export default pool; 
+ */
